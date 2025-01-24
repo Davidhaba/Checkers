60de58a12d10713808546dcfe78faae6e3d72aca
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2025-01-24 23:35:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -8,6 +9,14 @@ socketio = SocketIO(app)
 @app.route('/')
 def index():
     return render_template('main.html')
+
+@app.route('/<path:filename>')
+def serve_file(filename):
+    file_path = os.path.join('checkVerifications', filename)
+    if os.path.exists(file_path):
+        return send_from_directory('checkVerifications', filename)
+    else:
+        return "No found"
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
