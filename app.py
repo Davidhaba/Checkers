c3bdad669fb24baa828bc1044c7edcb4321a96db
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2025-01-24 21:50:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, leave_room, emit
+import eventlet
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -37,4 +39,4 @@ def handle_move(data):
     emit('update_board', data, room=room)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
